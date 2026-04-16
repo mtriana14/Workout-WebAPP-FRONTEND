f131dc47b94b980b83fbe433ffc885d459f9c5bf
mtriana14 <melanicapps@gmail.com>
2026-04-16 02:05:20 -0400

    api

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000";
 const COMPANY_ID = process.env.NEXT_PUBLIC_COMPANY_ID ?? "1";
 
 interface RequestOptions {
